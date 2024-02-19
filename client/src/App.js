58af668afd96e8549f4828210de9acb86f2d8010
sanyasappadu <sanyasappadu4@gmail.com>
2024-02-20 00:08:01 +0530

    menu is ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
-import Navbar from "./components/Navbar";
+import React, {useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import SignIn from "./pages/SignIn";
 
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+
         <BrowserRouter>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+
+        <Main>
             <Navbar />
+            <Wrapper>
               <Routes>
                 <Route path="/">
                   <Route index element={<Home />} />
@@ -18,8 +43,15 @@ function App() {
                   </Route>
                 </Route>
               </Routes>
+              </Wrapper>
+          </Main>
         </BrowserRouter>
+        </Container>
+
+        </ThemeProvider>
+
   );
 }
 
 export default App;
+
