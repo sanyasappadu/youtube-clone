cea5b5faaa0290e3df0214a5edcc77d8a58de793
sanyasappadu <sanyasappadu4@gmail.com>
2024-02-19 23:52:31 +0530

    navbar and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import SignIn from "./pages/SignIn";
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+        <BrowserRouter>
+            <Navbar />
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+        </BrowserRouter>
   );
 }
 
